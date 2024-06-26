8f55a058b813e2f5256fc99a0ca5c451c4f1a6e1
ChulumancoMbete <chulumancombete50@gmail.com>
2024-06-26 09:47:51 +0200

    initial commit
    Unity prototype 1 - Player Control

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    [SerializeField] Vector3 offset = new Vector3(0, 5, -7);
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Offset the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + offset;
+    }
+}
